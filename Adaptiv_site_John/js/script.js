467d2d28d90c8298f53797b23798da159aecb8a7
Dmitry Bokhan <iteushm@gmail.com>
2021-03-05 15:17:37 +0300

    Исправил бургер

--- Adaptiv_site_John/js/script.js
+++ Adaptiv_site_John/js/script.js
@@ -1,1 +1,13 @@
 // alert('Hello Gulp!');
+
+//**** МЕНЮ БУРГЕР****/
+//добавляем класс active к icon-menu и header__menu при событии click
+$(document).ready(function () {
+   $('.icon-menu').click(function (e) {
+      $('.menu__icon,.menu__body').toggleClass('_active');
+   });
+   $('.menu__link').click(function (e) {
+      $('.menu__icon,.menu__body').toggleClass('_active');
+   });
+});
+//**** МЕНЮ БУРГЕР конец кода****/
